2265e9fcd591db57b6f3628da8aec311f6545b88
Stepan Skichko <st.skichko@gmail.com>
2024-01-22 14:24:15 +0200

    add some assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,29 @@
+import bell from './bell.svg';
+import user from './user.svg';
+import favorite from './favorite.svg';
+import menu from './menu.svg';
+
+import logoMobile from './logo-mobile.svg';
+import logoDesktop from './logo-desktop.svg';
+
+import category1 from './category1.svg';
+import category2 from './category2.svg';
+import category3 from './category3.svg';
+import category4 from './category4.svg';
+import category5 from './category5.svg';
+import category6 from './category6.svg';
+
+export {
+  bell,
+  user,
+  favorite,
+  menu,
+  logoMobile,
+  logoDesktop,
+  category1,
+  category2,
+  category3,
+  category4,
+  category5,
+  category6,
+};
---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+// import bigShoe1 from './big-shoe1.png'
+
+// export {
+
+//     thumbnailShoe3
+// }
