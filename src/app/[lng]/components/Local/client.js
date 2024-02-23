841a31fbde1040db20409124baa9dfcd8043a064
Stepan Skichko <st.skichko@gmail.com>
2024-02-23 13:00:33 +0200

    delete i18n trash

--- src/app/[lng]/components/Local/client.js
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { LocalBase } from './LocalBase';
-import { useTranslation } from '../../../i18n/client';
-
-export const Local = ({ lng }) => {
-  const { t } = useTranslation(lng);
-  return <LocalBase t={t} lng={lng} />;
-};
--- src/app/[lng]/components/Local/index.js
+++
@@ -1,7 +0,0 @@
-import { useTranslation } from '../../../i18n';
-import { LocalBase } from './LocalBase';
-
-export const Local = async ({ lng }) => {
-  const { t } = await useTranslation(lng, 'footer');
-  return <LocalBase t={t} lng={lng} />;
-};
